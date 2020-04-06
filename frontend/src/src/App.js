f34b51ec390c382031425e0103ddd8544ee36f69
Joao Sironi <52471980+JoaoSironi@users.noreply.github.com>
2020-04-06 15:54:31 -0300

    Finalização do pagina de Login e de Registro

---
+++ frontend/src/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './global.css'
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+    );
+  };
+  
+  export default App;
+
+
+  // useState retorna um array [valor da variavel, função de atualização]
+  //JSX (JavaScript XML), quando o HTML é escrito em um arquivo .js
