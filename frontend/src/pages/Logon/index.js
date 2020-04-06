387ba80c3e905090b081efcf312a055741346338
Joao Sironi <joaovsironi@gmail.com>
2020-04-06 16:13:24 -0300

    fix

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
-import logoImg from '../../assets/logo.svg'
-import heroesImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
     return (
@@ -15,12 +16,12 @@ export default function Logon() {
                     <h1>Faça seu Login</h1>
 
                     <input placeholder = "Seu ID"/>
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
 
-                    <a href="/resgister">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="heroes"/>
